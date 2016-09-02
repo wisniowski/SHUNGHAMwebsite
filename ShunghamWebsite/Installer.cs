6b0beb01c7201f28abae797e15a525c705fe66f6
Aneliya <aneliyap.petkova@gmail.com>
2016-09-02 21:42:52 +0300

    Added Installer class for registering custom controls, layouts and resources on ApplicationStart.

---
+++ ShunghamWebsite/Installer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Telerik.Sitefinity.Localization;
+using Telerik.Sitefinity.Services;
+
+namespace SitefinityWebApp
+{
+    /// <summary>
+    /// Public class for registering custom components in Sitefinity.
+    /// </summary>
+    public class Installer
+    {
+        /// <summary>
+        /// Method that is called by ASP.NET before application start to install the custom controls on the site
+        /// </summary>
+        public static void PreApplicationStart()
+        {
+            SystemManager.ApplicationStart += SystemManager_ApplicationStart;
+        }
+
+        private static void SystemManager_ApplicationStart(object sender, EventArgs e)
+        {
+            Res.RegisterResource<ShunghamResources>();
+        }
+    }
+}
