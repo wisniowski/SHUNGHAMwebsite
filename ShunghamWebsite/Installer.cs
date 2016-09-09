25bb272c6a069d110925cd582025949a96c0c65c
Aneliya <aneliyap.petkova@gmail.com>
2016-09-09 13:20:32 +0300

    Added methods in the Installer for registering Sections and Controls in the Toolbox.

--- ShunghamWebsite/Installer.cs
+++ ShunghamWebsite/Installer.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Configuration;
 using Telerik.Sitefinity.Localization;
+using Telerik.Sitefinity.Modules.Pages.Configuration;
 using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.Web.UI;
 
 namespace SitefinityWebApp
 {
@@ -20,6 +24,82 @@ namespace SitefinityWebApp
         private static void SystemManager_ApplicationStart(object sender, EventArgs e)
         {
             Res.RegisterResource<ShunghamResources>();
+
+            RegisterSectionInBackend(Installer.pageControlsToolboxName, Installer.ShunghamControlsSectionName);
+            RegisterSectionInBackend(Installer.pageLayoutsToolboxName, Installer.ShunghamLayoutsName);
         }
+
+        private static void RegisterSectionInBackend(string toolboxName, string sectionName)
+        {
+            var configManager = ConfigManager.GetManager();
+            var config = configManager.GetSection<ToolboxesConfig>();
+
+            var controls = config.Toolboxes[toolboxName];
+
+            var section = controls.Sections.Where<ToolboxSection>(e => e.Name == sectionName).FirstOrDefault();
+
+            if (section == null)
+            {
+                configManager.Provider.SuppressSecurityChecks = true;
+                section = new ToolboxSection(controls.Sections)
+                {
+                    Name = sectionName,
+                    Title = sectionName,
+                    Description = sectionName,
+                    ResourceClassId = string.Empty
+                };
+                controls.Sections.Add(section);
+                configManager.SaveSection(config);
+
+                configManager.Provider.SuppressSecurityChecks = false;
+            }
+        }
+
+        private static void RegisterControlInToolbox(string toolboxName, string sectionName, string controlType, string controlName)
+        {
+            RegisterControlInToolbox(toolboxName, sectionName, controlType, controlName, string.Empty);
+        }
+
+        private static void RegisterControlInToolbox(string toolboxName, string sectionName, string controlType, string controlName, string layoutTemplate)
+        {
+            var configManager = ConfigManager.GetManager();
+            configManager.Provider.SuppressSecurityChecks = true;
+            var config = configManager.GetSection<ToolboxesConfig>();
+
+            var controls = config.Toolboxes[toolboxName];
+            var section = controls.Sections.Where<ToolboxSection>(e => e.Name == sectionName).FirstOrDefault();
+
+            if (section != null && !section.Tools.Any<ToolboxItem>(e => e.Name == controlName))
+            {
+                var tool = new ToolboxItem(section.Tools)
+                {
+                    Name = controlName,
+                    Title = controlName,
+                    Description = controlName,
+                    ControlType = controlType,
+                };
+
+                if (!string.IsNullOrEmpty(layoutTemplate))
+                {
+                    tool.LayoutTemplate = layoutTemplate;
+                }
+                section.Tools.Add(tool);
+
+                configManager.SaveSection(config);
+            }
+
+            configManager.Provider.SuppressSecurityChecks = false;
+        }
+
+        #region Private fields and constants
+
+        private const string pageControlsToolboxName = "PageControls";
+        private const string pageLayoutsToolboxName = "PageLayouts";
+        private const string ShunghamLayoutsName = "ShunghamLayouts";
+        private const string ShunghamControlsSectionName = "ShunghamControls";
+
+        private static readonly string layoutControlTypeName = typeof(LayoutControl).AssemblyQualifiedName;
+
+        #endregion
     }
 }
