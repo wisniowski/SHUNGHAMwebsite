b9ac3c9c2d1a6cdec3c1c397754a22f93c2a1ba1
AneliyaPPetkova <aneliyap.petkova@gmail.com>
2016-09-12 18:26:28 +0300

    FooterWidget - added code for applying Navigation Url to Social Share button links.

--- ShunghamWebsite/CustomWidgets/FooterWidget/FooterWidget.ascx.cs
+++ ShunghamWebsite/CustomWidgets/FooterWidget/FooterWidget.ascx.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace SitefinityWebApp.CustomWidgets.FooterWidget
 {
     public partial class FooterWidget : System.Web.UI.UserControl
     {
+        public string LinkedInButtonExternalLink { get; set; }
+        public string TwitterButtonExternalLink { get; set; }
+        public string FacebookButtonExternalLink { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            SetSocialShareButtonsDestination();
+        }
 
+        private void SetSocialShareButtonsDestination()
+        {
+            this.SetButtonUrl(this.LinkedInLink, LinkedInButtonExternalLink);
+            this.SetButtonUrl(this.TwitterLink, TwitterButtonExternalLink);
+            this.SetButtonUrl(this.FacebookLink, FacebookButtonExternalLink);
+        }
+
+        private void SetButtonUrl(HyperLink hyperLink, string buttonExternalLink)
+        {
+            if (!string.IsNullOrEmpty(buttonExternalLink))
+            {
+                hyperLink.NavigateUrl = buttonExternalLink;
+            }
         }
     }
 }
