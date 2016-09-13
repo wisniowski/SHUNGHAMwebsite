417d32a090eed725466a622bf2b7586b270551ff
AneliyaPPetkova <aneliyap.petkova@gmail.com>
2016-09-13 14:42:12 +0300

    ShunghamUtilities - added LibrariesUtilities class that is responsible for operating with Sitefinity Libraries and Media.

---
+++ ShunghamUtilities/LibrariesUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Modules.Libraries;
+
+namespace ShunghamUtilities
+{
+    public class LibrariesUtilities
+    {
+        public static string GetMediaUrlByImageId(Guid masterImageId, bool resolveAsAbsolutUrl)
+        {
+            var manager = LibrariesManager.GetManager();
+
+            // Get the master version of the image
+            var image = manager.GetImages().FirstOrDefault(i => i.Id == masterImageId);
+
+            var mediaUlr = String.Empty;
+
+            if (image != null)
+            {
+                // Resolve the media URL
+                mediaUlr = image.ResolveMediaUrl(resolveAsAbsolutUrl);
+            }
+
+            return mediaUlr;
+        }
+    }
+}
