ff4d4265d9d331e8686978e2ce8dfcf5f924c910
Veronica <veronica.milcheva@gmail.com>
2016-09-19 18:44:27 +0100

    Added custom error pages
    Added functionality for 404 and 500 error pages

---
+++ ShunghamWebsite/Global.asax.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace SitefinityWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            HttpException httpException = exception as HttpException;
+
+            if (!Request.IsLocal && httpException != null)
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 500:
+                        Response.Redirect("~/error/500");
+                        break;
+
+                    case 404:
+                        Response.Redirect("~/error/404");
+                        break;
+                }
+            }
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
