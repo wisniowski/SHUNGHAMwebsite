c965637ca62624f3bcf9a4c568338725cad06730
AneliyaPPetkova <aneliyap.petkova@gmail.com>
2016-09-21 12:10:53 +0300

    ButtonWidget is binded to data.

--- ShunghamWebsite/CustomWidgets/ButtonWidget/ButtonWidget.ascx.cs
+++ ShunghamWebsite/CustomWidgets/ButtonWidget/ButtonWidget.ascx.cs
@@ -1,17 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using ShunghamUtilities;
 
 namespace SitefinityWebApp.CustomWidgets.ButtonWidget
 {
     public partial class ButtonWidget : System.Web.UI.UserControl
     {
+        public string Text { get; set; }
+        public Guid LandingPageId { get; set; }
+        public string ExternalLink { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            BindButtonWidget();
+        }
 
+        private void BindButtonWidget()
+        {
+            if (!string.IsNullOrEmpty(this.Text))
+            {
+                this.ButtonTextLtl.Text = this.Text;
+            }
+
+            if (this.LandingPageId != null && this.LandingPageId != Guid.Empty)
+            {
+                var pageNodeUrl = PagesUtilities.GetPageUrlById(this.LandingPageId);
+                this.ButtonLink.NavigateUrl = pageNodeUrl;
+            }
+            else if (!string.IsNullOrEmpty(this.ExternalLink))
+            {
+                this.ButtonLink.NavigateUrl = this.ExternalLink;
+            }
         }
     }
 }
